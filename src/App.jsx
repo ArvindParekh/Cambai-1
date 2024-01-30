f8c6afaaa2e71355f03d3c41442ba8d2bffcfa95
Arvind <aruparekh@gmail.com>
2024-01-30 09:37:06 +0530

    Initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalculatorCard from "./app/CalculatorCard"
+
+function App() {
+
+  return (
+    <main className="w-[100vw] h-[100vh] flex items-center justify-center">
+       <CalculatorCard />
+    </main>
+  )
+}
+
+export default App
---
+++ src/app/CalculatorCard.jsx
@@ -0,0 +1,13 @@
+
+
+const CalculatorCard = ()=>{
+
+
+    return (
+        <>
+            <h1>Helo</h1>
+        </>
+    )
+}
+
+export default CalculatorCard;
