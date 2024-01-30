cc10f3a7116e8a58e175c070ebf60b0485f8d321
Arvind <aruparekh@gmail.com>
2024-01-30 10:30:59 +0530

    feat: add calculator outline and required functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CalculatorCard from "./app/CalculatorCard"
 function App() {
 
   return (
-    <main className="w-[100vw] h-[100vh] flex items-center justify-center">
+    <main className="w-[100vw] h-[100vh] flex items-center justify-center bg-gray-100">
        <CalculatorCard />
     </main>
   )
--- src/app/CalculatorCard.jsx
+++ src/app/CalculatorCard.jsx
@@ -1,13 +1,153 @@
+import { useState } from "react";
 
+const CalculatorCard = () => {
+  const [inputValue, setInputValue] = useState(undefined);
+  const [first, setFirst] = useState();
+  const [second, setSecond] = useState();
 
-const CalculatorCard = ()=>{
+  function handleClick(event) {
+    const value = event.target.getAttribute("data-value");
+    if (value === "=") {
+      handleSubmit();
+    } else {
+      if (inputValue === undefined) {
+        setInputValue(value);
+      } else {
+        setInputValue((prev) => prev + value);
+      }
+    }
+  }
 
+  function handleSubmit(){
+    console.log(eval(inputValue), inputValue);
+  }
 
-    return (
-        <>
-            <h1>Helo</h1>
-        </>
-    )
-}
+  return (
+    <aside className="border bg-white rounded-xl shadow-2xl p-5">
+      <div className="flex flex-col items-center gap-5">
+        <h1 className="text-6xl font-bold">Calculator</h1>
+        <input
+          className="p-2 border text-xl font-medium rounded-lg"
+          value={inputValue}
+          readOnly
+        />
+        <div className="grid grid-rows-4 grid-cols-4 gap-4 w-full text-center">
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={1}
+            onClick={handleClick}
+          >
+            1
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={2}
+            onClick={handleClick}
+          >
+            2
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={3}
+            onClick={handleClick}
+          >
+            3
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"/"}
+            onClick={handleClick}
+          >
+            /
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={4}
+            onClick={handleClick}
+          >
+            4
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={5}
+            onClick={handleClick}
+          >
+            5
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={6}
+            onClick={handleClick}
+          >
+            6
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"*"}
+            onClick={handleClick}
+          >
+            *
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={7}
+            onClick={handleClick}
+          >
+            7
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={8}
+            onClick={handleClick}
+          >
+            8
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={9}
+            onClick={handleClick}
+          >
+            9
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"-"}
+            onClick={handleClick}
+          >
+            -
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={0}
+            onClick={handleClick}
+          >
+            0
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"."}
+            onClick={handleClick}
+          >
+            .
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"="}
+            onClick={handleClick}
+          >
+            =
+          </button>
+          <button
+            className="border p-4 rounded-lg border-1"
+            data-value={"+"}
+            onClick={handleClick}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
 
 export default CalculatorCard;
